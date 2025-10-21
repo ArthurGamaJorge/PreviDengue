788f0f095f31b3bb1ecf2aac56e92c6b95d159dd
ionmateus <ionmateusoprea@gmail.com>
2025-10-21 08:03:54 -0300

    pull

--- website/src/lib/config.ts
+++ website/src/lib/config.ts
@@ -1,3 +1,3 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "https://previdengue-previdengueapi.hf.space";
 // http://localhost:8000
 // https://previdengue-previdengueapi.hf.space
